dee6bb900cb5c762e14a1cc304a75fec417811c5
Ayman <aymanibrahim678@gmail.com>
2025-07-19 01:13:42 +0300

    0

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,20 +1,28 @@
 import { prisma } from "@/lib/prisma";
-// import { cache } from "react";
 
-// const getCachedPost = cache((slug: string) => {
-//   return prisma.post.findUnique({
-//     where: { slug },
-//   });
-// });
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
 
-const PostPage = async ({ params }: { params: { slug: string } }) => {
+const PostPage = async ({ params }: PageProps) => {
   const post = await prisma.post.findUnique({
     where: { slug: params.slug },
   });
+
+  if (!post) {
+    return (
+      <main className="flex flex-col items-center justify-center pt-8 text-center">
+        <h1 className="text-2xl font-semibold">Post not found</h1>
+      </main>
+    );
+  }
+
   return (
     <main className="flex flex-col items-center gap-y-5 pt-8 text-center">
-      <h1 className="text-3xl font-semibold">{post?.title}</h1>
-      <p>{post?.content}</p>
+      <h1 className="text-3xl font-semibold">{post.title}</h1>
+      <p>{post.content}</p>
     </main>
   );
 };
